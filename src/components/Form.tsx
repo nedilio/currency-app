e37c257d75ef7be5206c81adaf5f02312c4529db
Nedilio <izquierdonelson@gmail.com>
2023-05-11 23:15:10 -0400

    🐛 fix: change to numeric keyboard

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,7 @@
 ﻿"use client";
 
 import { Country } from "@/types/types";
+import { on } from "events";
 import React from "react";
 
 interface Props {
@@ -10,25 +11,17 @@ interface Props {
 }
 
 function Form({ value, onChange, country }: Props) {
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value.replace(",", ".");
-    if (value === "") {
-      value = "0";
-    }
-    onChange(Number(value));
-  };
-
   return (
     <form className="w-full">
       <label className="block space-y-1 text-lg">
         <span className="block text-center">Monto en {country.currency}:</span>
         <input
           className="block w-full rounded-full bg-green-700 p-2 text-right text-3xl"
-          type="text"
+          type="number"
           min={0}
           value={value}
-          inputMode="decimal"
-          onChange={handleOnChange}
+          inputMode="numeric"
+          onChange={(e) => onChange(Number(e.target.value))}
         />
       </label>
     </form>
